3cef1807ec624e93d474dc44ba4bab966463ea7b
Adarsh Singh <cradarsh205@gmail.com>
2026-05-02 18:46:34 +0530

    product import issue  fixed

--- server/controller/product.js
+++ server/controller/product.js
@@ -1,4 +1,4 @@
-import { Product } from "../models/product.js";
+import { Product } from "../models/Product.js";
 import TryCatch from "../utils/tryCatch.js";
 import bufferGenerator from "../utils/bufferGenerator.js";
 import cloudinary from "cloudinary";
--- server/models/Product.js
+++ server/models/Product.js
@@ -39,4 +39,4 @@ const productSchema =new mongoose.Schema({
 
 })
 
-export const Product = mongoose.model("Product" , productSchema);
+export const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
